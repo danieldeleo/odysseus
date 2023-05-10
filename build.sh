ab4db508e8dc52f4c780876a698b9bf91df59f1d
ddeleo <danieldeleo@google.com>
2023-05-10 16:45:52 -0400

    update

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-# python3 -m pip install -r python/arch_diagram_parser_local_controller/requirements.txt
-# python3 python/arch_diagram_parser_local_controller/parse.py
+python3 -m pip install -r python/arch_diagram_parser_local_controller/requirements.txt
+python3 python/arch_diagram_parser_local_controller/parse.py
